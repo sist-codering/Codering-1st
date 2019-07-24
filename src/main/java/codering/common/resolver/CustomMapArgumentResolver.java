7ee0393afc6a46694aad066c700d2d73e37eec5d
kimjungkyu93 <kjg2588@gmail.com>
2019-07-24 11:48:58 +0900

    [2019-07-24]
    [간단한수정] 괄호닫아줌

--- src/main/java/codering/common/resolver/CustomMapArgumentResolver.java
+++ src/main/java/codering/common/resolver/CustomMapArgumentResolver.java
@@ -22,9 +22,9 @@ public class CustomMapArgumentResolver implements HandlerMethodArgumentResolver
 {
 	@Autowired
 	private AlarmDAO alarmDao;
-	
+
 	private static final Log log = LogFactory.getLog(CustomMapArgumentResolver.class);
-			
+
 	@Override
 	public boolean supportsParameter(MethodParameter parameter)
 	{
@@ -37,41 +37,40 @@ public class CustomMapArgumentResolver implements HandlerMethodArgumentResolver
 			NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception
 	{
 		CommandMap commandMap = new CommandMap();
-		
-		HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
-		
-		// 로그인세션이 존재한다면 그것을 commandMap에도 담음
-		
-		
-		  Map<String, Object> loginInfo = (Map<String, Object>)
-		  request.getSession().getAttribute("loginInfo");
-		  
-		  if(loginInfo != null) { commandMap.put("loginInfo", loginInfo);
-		  commandMap.put("MEMBER_ID", loginInfo.get("MEMBER_ID"));
-		  commandMap.put("NICKNAME", loginInfo.get("NICKNAME"));
-		  commandMap.put("TUTOR_ID", loginInfo.get("TUTOR_ID"));
-		  commandMap.put("TUTOR_NAME", loginInfo.get("TUTOR_NAME"));
-		 
-		  
-		  // 현재 알람이 몇개인지 확인하여 세션에 넣기 request.getSession().setAttribute("alarmCount",
-		 alarmDao.selectAlarmCount(commandMap.getMap());  
-		 
-		
-		// 파라메터들을 수신하여 commandMap에 담는 과정
-		Enumeration<?> enumeration = request.getParameterNames();		
-		String key = null;		
-		String[] values = null;		
-		while (enumeration.hasMoreElements())
-		{			
-			key = (String) enumeration.nextElement();			
-			values = request.getParameterValues(key);			
-			if (values != null)
-			{
-				commandMap.put(key, (values.length > 1) ? values : values[0]);
-			}			
-		}		
-		// 담아놓은 commandMap 반납
-		return commandMap;
-	}
 
+		HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+
+		// 로그인세션이 존재한다면 그것을 commandMap에도 담음
+
+		Map<String, Object> loginInfo = (Map<String, Object>) request.getSession().getAttribute("loginInfo");
+
+		if (loginInfo != null)
+		{
+			commandMap.put("loginInfo", loginInfo);
+			commandMap.put("MEMBER_ID", loginInfo.get("MEMBER_ID"));
+			commandMap.put("NICKNAME", loginInfo.get("NICKNAME"));
+			commandMap.put("TUTOR_ID", loginInfo.get("TUTOR_ID"));
+			commandMap.put("TUTOR_NAME", loginInfo.get("TUTOR_NAME"));
+
+			// 현재 알람이 몇개인지 확인하여 세션에 넣기 request.getSession().setAttribute("alarmCount",
+			alarmDao.selectAlarmCount(commandMap.getMap());
+
+			// 파라메터들을 수신하여 commandMap에 담는 과정
+			Enumeration<?> enumeration = request.getParameterNames();
+			String key = null;
+			String[] values = null;
+			while (enumeration.hasMoreElements())
+			{
+				key = (String) enumeration.nextElement();
+				values = request.getParameterValues(key);
+				if (values != null)
+				{
+					commandMap.put(key, (values.length > 1) ? values : values[0]);
+				}
+			}
+			// 담아놓은 commandMap 반납
+			return commandMap;
+		}
+		return loginInfo;
+	}
 }
